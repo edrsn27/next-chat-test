0ab9219d28b063136150f662e548c4dfb5b3a100
ederson <edersonvillegas50@gmail.com>
2022-02-09 04:25:04 +0800

    initial jwt

---
+++ plugins/apollo-client.js
@@ -0,0 +1,48 @@
+import * as Realm from "realm-web";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+
+import fetch from "isomorphic-unfetch";
+
+const app = Realm.App.getApp(process.env.REALM_APP_ID);
+
+const graphql_url = `https://realm.mongodb.com/api/client/v2.0/app/${process.env.REALM_APP_ID}/graphql`;
+
+async function loginApiKey(apiKey) {
+  // Create an API Key credential
+  const credentials = Realm.Credentials.apiKey(apiKey);
+  try {
+    // Authenticate the user
+    const user = await app.logIn(credentials);
+    // `App.currentUser` updates to match the logged in user
+    return user;
+  } catch (err) {
+    console.error("Failed to log in", err);
+  }
+}
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: graphql_url,
+    fetch: async (uri, options) => {
+      const accessToken = (await loginApiKey(process.env.REALM_API_KEY))
+        .accessToken;
+      options.headers.Authorization = `Bearer ${accessToken}`;
+      return fetch(uri, options);
+    },
+  }),
+  cache: new InMemoryCache({
+    addTypename: false,
+  }),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "ignore",
+    },
+    query: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "all",
+    },
+  },
+});
+
+export default client;
